41cd72be4c35f135bea6c80cb3e85b043126c122
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-02 23:04:10 +0900

    feat: item 관련 api 호출 함수 작성

---
+++ src/api/item.ts
@@ -0,0 +1,45 @@
+import { itemApiInstance } from '@/api/interceptor';
+import { END_POINT } from '@/constants/api';
+import { ItemForm } from '@/types/Item';
+
+export const getItemList = async () => {
+  const { data } = await itemApiInstance.get(`${END_POINT.ITEM}`);
+  return data;
+};
+
+export const getItem = async (itemId: string) => {
+  const { data } = await itemApiInstance.get(END_POINT.ITEM(itemId));
+  return data;
+};
+
+export const getItemKeyword = async (keyword: string) => {
+  const { data } = await itemApiInstance.get(END_POINT.ITEM_KEYWORD(keyword));
+  return data;
+};
+
+export const postItem = async (
+  form: Pick<ItemForm, 'name' | 'tags' | 'image'>,
+) => {
+  const { data } = await itemApiInstance.post(END_POINT.REGISTER_ITEM, form);
+  return data;
+};
+
+export const patchItemStatus = async (itemId: string, status: boolean) => {
+  const { data } = await itemApiInstance.patch(
+    `${END_POINT.ADMIM_UPDATE}/${itemId}`,
+    { status },
+  );
+  return data;
+};
+
+export const deleteItem = async (itemId: string) => {
+  const { data } = await itemApiInstance.patch(
+    `${END_POINT.ADMIN_DELETE}/${itemId}`,
+  );
+  return data;
+};
+
+export const deleteItems = async () => {
+  const { data } = await itemApiInstance.patch(`${END_POINT.ADMIN_DELETE_ALL}`);
+  return data;
+};
