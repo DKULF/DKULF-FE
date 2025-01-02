44114a2c05435331e69ddc944de842d790e6ec4e
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-02 23:03:58 +0900

    feat: auth 관련 api 호출 함수 작성

---
+++ src/api/auth.ts
@@ -0,0 +1,37 @@
+import { authApiInstance } from '@/api/interceptor';
+import { END_POINT } from '@/constants/api';
+import { MemberForm } from '@/types/member';
+
+export const postEmailRequest = async (email: string) => {
+  const { data } = await authApiInstance.post(END_POINT.EMAIL_REQUEST, {
+    email,
+  });
+  return data;
+};
+
+export const postEmailVerify = async (email: string, code: number) => {
+  const { data } = await authApiInstance.post(END_POINT.EMAIL_VERIFY, {
+    email,
+    code,
+  });
+  return data;
+};
+
+export const postMember = async (
+  form: Pick<MemberForm, 'nickname' | 'email' | 'password'>,
+) => {
+  const { data } = await authApiInstance.post(`${END_POINT.SIGN_UP}`, form);
+  return data;
+};
+
+export const postLogin = async (
+  form: Pick<MemberForm, 'email' | 'password'>,
+) => {
+  const { data } = await authApiInstance.post(`${END_POINT.LOGIN}`, form);
+  return data;
+};
+
+export const getMember = async () => {
+  const { data } = await authApiInstance.get(`${END_POINT.USER}`);
+  return data;
+};
