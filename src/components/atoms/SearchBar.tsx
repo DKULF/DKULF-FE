4e2a2907b0d75a3e3b139f764fe7e62d5d92617e
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-03 01:02:20 +0900

    style: props로 className 받아오도록 추가

--- src/components/atoms/SearchBar.tsx
+++ src/components/atoms/SearchBar.tsx
@@ -7,7 +7,7 @@ export const SearchBar = () => {
       <input
         type="search"
         placeholder="습득물 또는 키워드를 검색해보세요."
-        className="w-full pl-10 pr-4 py-3 rounded-lg border focus:outline-none"
+        className="w-full pl-10 pr-4 py-3 rounded-lg border focus:outline-none placeholder:text-sm"
       />{' '}
     </div>
   );
