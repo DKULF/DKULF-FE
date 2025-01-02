ae40ab3ff6088663aa9ef12795f7eedb28ad360e
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-02 23:27:31 +0900

    feat: 유실물 등록 API 호출 훅 작성

---
+++ src/hooks/api/item/useRegisterItem.ts
@@ -0,0 +1,22 @@
+import { postItem } from '@/api/item';
+import { ItemForm } from '@/types/Item';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+
+export const useRegisterItem = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: (data: ItemForm) => postItem(data),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['items', 'list'] });
+      alert('등록되었습니다!');
+      navigate('/');
+    },
+    onError: (error) => {
+      console.error(error);
+      alert('등록에 실패했습니다. 다시 시도해주세요.');
+    },
+  });
+};
