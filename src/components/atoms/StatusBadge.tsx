5d0c4252feadab23a497415d6c53e9ce7cb8cd4c
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-03 16:32:46 +0900

    feat: 어드민 기능 추가를 위한 clickhandler 수정

--- src/components/atoms/StatusBadge.tsx
+++ src/components/atoms/StatusBadge.tsx
@@ -1,10 +1,12 @@
 interface StatusBadgeProps {
   status: boolean;
+  onClick?: () => void;
 }
 
-export const StatusBadge = ({ status }: StatusBadgeProps) => {
+export const StatusBadge = ({ status, onClick }: StatusBadgeProps) => {
   return (
     <div
+      onClick={onClick}
       className={`${status ? 'bg-green-50 text-green-600' : 'bg-[#dfdfdf] text-[#666666]'} text-xs font-semibold px-2 py-[5px] rounded-[30px] inline-flex items-center`}
     >
       <span>{status === true ? '보관중' : '수령완료'}</span>
